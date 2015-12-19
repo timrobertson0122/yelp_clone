e6f39d640fcdb5fbf43b9636f5b4425e6b88fe33
Tim <timrobertson0122@gmail.com>
2015-12-19 14:15:16 +0100

    passing test for creating new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,20 @@
 class RestaurantsController < ApplicationController
 
+	def restaurant_params
+		params.require(:restaurant).permit(:name)
+	end
+
 	def index
 		@restaurants = Restaurant.all
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
 	end
 
 end
