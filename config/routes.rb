b16e9e616d181ba03c39aee63178f5c32ba9b592
Tim <timrobertson0122@gmail.com>
2015-12-21 16:50:33 +0100

    added endorsements controller and relationship to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
 	root to: 'restaurants#index'
 
-	resources :restaurants do
-		resources :reviews
+	resources :restaurants, shallow: true do
+		resources :reviews do
+			resources :endorsements
+		end
 	end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
