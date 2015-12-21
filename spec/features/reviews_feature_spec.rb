a1b135688df08df62a84c96ebf95e3960186e950
Tim <timrobertson0122@gmail.com>
2015-12-21 13:15:48 +0100

    adding average ratings

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,10 +3,15 @@ require_relative 'helpers/session'
 include SessionHelpers
 
 feature 'reviewing' do
-	before {Restaurant.create name: 'KFC'}
+
+	let!(:restaurant) { FactoryGirl.create(:restaurant) }
+	let!(:user) { FactoryGirl.create(:user) }
+
+	before do
+		login_as(user, :scope => :user)
+	end
 
 	scenario 'allows logged in users to leave a review using a form' do
-		sign_in('test@example.com', 'testtest')
 		visit '/restaurants'
 		click_link 'Review KFC'
 		fill_in "Thoughts", with: "so, so"
@@ -16,15 +21,7 @@ feature 'reviewing' do
 		expect(page).to have_content("so, so")
 	end
 
-	scenario 'prevents non logged in users from leaving a review' do
-		visit '/restaurants'
-		click_link 'Review KFC'
-		expect(page).to have_content 'Log in'
-		expect(page).not_to have_content 'Thoughts'
-	end
-
 	scenario 'allows just one review per user per restaurant' do
-		sign_in('test@example.com', 'testtest')
 		leave_review('so, so', '1')
 		leave_review('amazeballs', '5')
 		expect(page).to have_content 'You have already reviewed this restaurant'
@@ -32,7 +29,6 @@ feature 'reviewing' do
 	end
 
 	scenario 'creator can delete the review' do
-		sign_in('test@example.com', 'testtest')
 		leave_review('so, so', '1')
 		visit '/restaurants'
 		click_link 'Delete review'
@@ -41,7 +37,6 @@ feature 'reviewing' do
 	end
 
 	scenario 'non creator cannot delete the review' do
-		sign_in('test@example.com', 'testtest')
 		leave_review('so, so', '1')
 		visit '/restaurants'
 		click_link 'Sign out'
@@ -51,4 +46,9 @@ feature 'reviewing' do
 		expect(page).to have_content 'You can only delete a review that you have created'
 	end
 
+	scenario 'displays an average rating for all reviews' do
+		leave_review('So so', '3')
+		leave_review('Great', '5')
+		expect(page).to have_content('Average rating: 4')
+	end
 end
